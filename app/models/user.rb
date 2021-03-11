552a6a55b6dd6c71af41ee84720dd04356b26009
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-03-11 22:06:10 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #エラーメッセージの順番を保つためwith_optionsを使用しない箇所あり
+  with_options presence: true do
+    validates :nickname
+    validates :profile
+    validates :dream
+    validates :first_limit_id, numericality: { other_than: 1 } 
+    validates :first_process
+    validates :second_limit_id, numericality: { other_than: 1 } 
+    validates :second_process
+    validates :third_limit_id, numericality: { other_than: 1 } 
+    validates :third_process
+    validates :fourth_limit_id, numericality: true
+    validates :fourth_process
+    validates :fifth_limit_id, numericality: true
+    validates :fifth_process
+  end
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :genres  #保留
 end
