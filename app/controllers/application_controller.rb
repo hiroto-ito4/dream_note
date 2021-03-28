73d604ab1615ad2a0070da857cca03ae29774f60
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-03-28 20:53:01 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: [:nickname, :profile, :dream, :first_limit_id, :first_process, :second_limit_id,
                                              :second_process, :third_limit_id, :third_process, :fourth_limit_id, :fourth_process, :fifth_limit_id, :fifth_process])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
