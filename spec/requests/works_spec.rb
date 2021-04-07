c898150cee7fb8a1ed59ddd6e0dde7fee7d7023c
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-04-07 22:43:46 +0900

    Update works_spec.rb

--- spec/requests/works_spec.rb
+++ spec/requests/works_spec.rb
@@ -7,6 +7,8 @@ describe WorksController, type: :request do
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get root_path
+      expect(response.status).to eq 200
     end
   end
 end
